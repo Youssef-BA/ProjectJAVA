47bbe84acb93535a85224bfd22f695455b30819c
yassinebakkali2 <yassine.bakkali-kasmi@esi.ac.ma>
2024-02-13 21:57:24 +0100

    retouches

---
+++ src/esi/oncf/data/Place.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package esi.oncf.data;
+
+/**
+ *
+ * @author yassi
+ */
+public class Place {
+    
+    private int numPlace;
+    private int idPlace;
+
+    public Place() {
+    }
+
+    
+    
+    public Place(int numPlace, int idPlace) {
+        this.numPlace = numPlace;
+        this.idPlace = idPlace;
+    }
+    
+    
+}
---
+++ src/esi/oncf/data/Train.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package esi.oncf.data;
+
+/**
+ *
+ * @author yassi
+ */
+public class Train {
+    
+    private String type;
+    private String numTrain;
+    
+    public Train(){};
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setNumTrain(String numTrain) {
+        this.numTrain = numTrain;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getNumTrain() {
+        return numTrain;
+    }
+    
+}
