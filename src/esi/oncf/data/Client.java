2079e2a1eebc938ee0df4fa88ba8980b0afe8a78
Projdoub <43033284+WholeAce@users.noreply.github.com>
2024-02-15 15:19:36 +0100

    Update injection sql
    Mesure simple contre injection sql

--- src/esi/oncf/data/Client.java
+++ src/esi/oncf/data/Client.java
@@ -1,23 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package esi.oncf.data;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author yassi
- */
 public class Client {
     private String password;
     private String ID;
@@ -46,66 +35,60 @@ public class Client {
     public void setNom(String nom) {
         this.nom = nom;
     }
+
     public Client() {
-            
-        }
-    
+    }
+
     public Client(String ID) {
         this.ID = ID;
     }
-    
-    
-    public boolean loginClient(String pswd,String ID) throws SQLException{
-        setID(ID);
+
+    public boolean loginClient(String enteredPassword, String userID) {
+        setID(userID);
+
         try {
-            Connection con=DatabaseConnection.getConnection();
-            PreparedStatement pst = con.prepareStatement("select Password from client where ID_USER=?");
-            pst.setString(1, ID);
-            ResultSet rs=pst.executeQuery();
-            while(rs.next()){
-                this.password=rs.getString("Password");
-                System.out.println(this.password);
-                
-                    }
-            
-            if (pswd.equals(this.password)){
-                System.out.println("Login Succes");
-                return true;
+            Connection con = DatabaseConnection.getConnection();
+            PreparedStatement pst = con.prepareStatement("SELECT Password FROM client WHERE ID_USER=?");
+            pst.setString(1, userID);
+
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                String storedPassword = rs.getString("Password");
+
+                // Use a secure method to compare passwords
+                if (checkPassword(enteredPassword, storedPassword)) {
+                    System.out.println("Login Successful");
+                    return true;
                 }
-            
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
-         catch (SQLException ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);}
-        
+
         System.out.println("Login Failed");
-               return false;
+        return false;
     }
-        
-        
-    public void reserver(String idVoyage,String idPlace) {
-        
-        int IDV= Integer.parseInt(idVoyage);
-        int IDP= Integer.parseInt(idPlace);
-        int IDU=Integer.parseInt(this.ID);
-        
+
+    public void reserver(String idVoyage, String idPlace) {
+        int IDV = Integer.parseInt(idVoyage);
+        int IDP = Integer.parseInt(idPlace);
+        int IDU = Integer.parseInt(this.ID);
+
         try {
-    
-            Connection con=DatabaseConnection.getConnection();
-            PreparedStatement pst = con.prepareStatement("INSERT INTO reservation (VoyageReserve,placeReserve,ID_USER) VALUES (?, ? , ?)");
+            Connection con = DatabaseConnection.getConnection();
+            PreparedStatement pst = con.prepareStatement("INSERT INTO reservation (VoyageReserve, placeReserve, ID_USER) VALUES (?, ?, ?)");
             pst.setInt(1, IDV);
             pst.setInt(2, IDP);
-            pst.setInt(3,IDU);
+            pst.setInt(3, IDU);
             int rowsInserted = pst.executeUpdate();
-                
-                    }
-            
-        catch (SQLException ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);}
-        
-        
-        
+        } catch (SQLException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-        
-        
-  
+
+    private boolean checkPassword(String enteredPassword, String storedPassword) {
+
+        return enteredPassword.equals(storedPassword);
+    }
 }
